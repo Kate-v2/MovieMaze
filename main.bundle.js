a1de00c5e0a3a15bc4741175c4479668a04e7c4e
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-27 06:45:55 -0700

    Build package

--- main.bundle.js
+++ main.bundle.js
@@ -54,7 +54,7 @@
 
 	var _nav_bar2 = _interopRequireDefault(_nav_bar);
 
-	var _welcome = __webpack_require__(7);
+	var _welcome = __webpack_require__(8);
 
 	var _welcome2 = _interopRequireDefault(_welcome);
 
@@ -315,6 +315,20 @@
 	    value: function isLoggedIn() {
 	      return this.token() != null || this.token() != undefined;
 	    }
+	  }, {
+	    key: 'setToken',
+	    value: function setToken(token) {
+	      sessionStorage['token'] = token;
+	    }
+	  }, {
+	    key: 'clearMovies',
+	    value: function clearMovies() {
+	      var token = this.token();
+	      sessionStorage.clear();
+	      if (token) {
+	        this.setToken(token);
+	      }
+	    }
 	  }]);
 
 	  return SessionTools;
@@ -355,7 +369,10 @@
 	      var title = document.getElementById('search-bar').value;
 	      var obj = { "term": title };
 	      var body = JSON.stringify(obj);
-	      var blob = new Blob([body], { type: 'application/json' });
+	      // let blob = new Blob( [ body ], {type : 'application/json'})
+	      // let blob = new Blob( body, {type: 'application/json'} )
+	      // let blob = new Blob( [body] )
+	      // let blob = new Blob( [body], {type: 'json'} )
 	      // let url  = `https://movie-maze.herokuapp.com/api/v1/search`
 	      var url = 'https://movie-maze.herokuapp.com/api/v1/search?term=' + title;
 
@@ -414,14 +431,20 @@
 	});
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	// import Movie     from '../classes/movie.js'
+
 
 	var _dom_tools = __webpack_require__(1);
 
 	var _dom_tools2 = _interopRequireDefault(_dom_tools);
 
-	var _movie = __webpack_require__(6);
+	var _session_tools = __webpack_require__(3);
 
-	var _movie2 = _interopRequireDefault(_movie);
+	var _session_tools2 = _interopRequireDefault(_session_tools);
+
+	var _movie_service = __webpack_require__(6);
+
+	var _movie_service2 = _interopRequireDefault(_movie_service);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -469,9 +492,14 @@
 
 	      var div = this.tool.newElement('div');
 	      this.tool.elementNames(div, 'ResultsContainer');
+
+	      var sesh = new _session_tools2.default();
+	      sesh.clearMovies();
+
 	      var elements = data.map(function (r) {
-	        return _this.media_card(r, div);
+	        _this.media_card(r, div);
 	      });
+	      // save user in var & clear session, restore user
 	      element.appendChild(div);
 	    }
 	  }, {
@@ -482,10 +510,15 @@
 	      this.tool.elementNames(span, id, 'mediaCard');
 	      var img = this.addMediaImage(data['picture'], span);
 	      var title = this.addMediaTitle(data['title'], span);
+	      var titleText = data['title'];
+	      var keyword = titleText.toUpperCase();
 
-	      // span.onClick = function() { new Movie(data).loadMovie() }
-	      //
-	      // This has to be a service callback
+	      var str = JSON.stringify(data['streams']);
+	      sessionStorage[keyword] = str;
+	      span.addEventListener('click', function () {
+	        var movie = new _movie_service2.default();
+	        movie.getMovie(titleText);
+	      });
 
 	      container.appendChild(span);
 	    }
@@ -546,6 +579,64 @@
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+	var _movie = __webpack_require__(7);
+
+	var _movie2 = _interopRequireDefault(_movie);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var MovieService = function () {
+	  function MovieService() {
+	    _classCallCheck(this, MovieService);
+	  }
+
+	  _createClass(MovieService, [{
+	    key: 'getMovie',
+	    value: function getMovie(title) {
+	      var obj = { "title": title };
+	      var body = JSON.stringify(obj);
+	      var blob = new Blob([body], { type: 'application/json' });
+
+	      var url = 'https://movie-maze.herokuapp.com/api/v1/movie?title=' + title;
+
+	      var movie = new _movie2.default();
+	      var data;
+	      var req = new XMLHttpRequest();
+	      req.open('GET', url, true);
+	      req.setRequestHeader("Content-Type", "application/json");
+	      req.setRequestHeader("Accept", "application/json");
+	      req.onload = function () {
+	        var stat = this.status;
+	        var valid = stat >= 200 && stat < 300;
+	        if (valid) {
+	          data = JSON.parse(this.responseText)['data']['attributes'];
+	          movie.loadMovie(data);
+	        }
+	        // else { console.log(this.error) }
+	      };
+	      req.send();
+	    }
+	  }]);
+
+	  return MovieService;
+	}();
+
+	exports.default = MovieService;
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 	var _dom_tools = __webpack_require__(1);
 
 	var _dom_tools2 = _interopRequireDefault(_dom_tools);
@@ -559,28 +650,288 @@
 	    _classCallCheck(this, Movie);
 
 	    this.data = data;
-	    this.streams = data['streams'];
-	    this.title = data['title'];
-	    this.img = data['picture'];
+	    // this.streams = data['streams']
+	    // this.title   = data['title']
+	    // this.title   = title
+	    // this.streams   = streams
+	    // this.img     = data['picture']
 
 	    this.tool = new _dom_tools2.default();
 	  }
 
 	  _createClass(Movie, [{
 	    key: 'loadMovie',
-	    value: function loadMovie() {
-	      var div = this.tool.getElement('content').bind(this);
+	    value: function loadMovie(raw) {
+	      var data = raw['details'];
+
+	      var div = this.tool.getElement('content');
 	      this.tool.clearElement(div);
+
+	      var container = this.movieContainer(div);
+
+	      var details = this.addDetails(data, container);
+	      // let img   = this.addPoster(data['Poster'], container)
+	      // let title = this.addTitle(data['Title'], container)
+	    }
+	  }, {
+	    key: 'movieContainer',
+	    value: function movieContainer(element) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, 'Movie', null);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addPoster',
+	    value: function addPoster(src, element) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, 'Poster', null);
+
+	      if (src) {
+	        var img = this.tool.newElement('img');
+	        img.src = src;
+	      } else {
+	        var img = this.tool.newElement('span');
+	        var p = this.tool.newElement('p');
+	        p.innerHTML = "No Image";
+	        img.appendChild(p);
+	      }
+
+	      span.appendChild(img);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addDetails',
+	    value: function addDetails(data, element) {
+	      var div = this.tool.newElement('div');
+	      this.tool.elementNames(div, 'Details', null);
+
+	      this.addPoster(data['Poster'], div);
+	      this.addContent(data, div);
+
+	      element.appendChild(div);
+	    }
+	  }, {
+	    key: 'addContent',
+	    value: function addContent(data, div) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, 'Text', null);
+
+	      var header = this.addHeader(data, span);
+	      var addSubHeader = this.addSubHeader(data, span);
+	      this.addGenre(data['Genre'], span);
+	      this.addActors(data['Actors'], span);
+	      this.addPlot(data['Plot'], span);
+	      this.addAwards(data['Awards'], span);
+	      this.addStreaming(data['Title'], span);
+
+	      div.appendChild(span);
+	    }
+	  }, {
+	    key: 'addHeader',
+	    value: function addHeader(data, element) {
+	      var header = this.tool.newElement('div');
+	      this.tool.elementNames(header, 'Header', null);
+
+	      this.addTitle(data['Title'], header);
+	      this.addYear(data['Year'], header);
+
+	      element.appendChild(header);
+	      return header;
+	    }
+	  }, {
+	    key: 'addTitle',
+	    value: function addTitle(title, element) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, 'Title', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = title;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addYear',
+	    value: function addYear(year, element) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, 'Year', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = year;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addSubHeader',
+	    value: function addSubHeader(data, element) {
+	      var sub = this.tool.newElement('div');
+	      this.tool.elementNames(sub, 'SubHeader', null);
+
+	      this.addRated(data['Rated'], sub);
+	      this.addRuntime(data['Runtime'], sub);
+	      this.addType(data['Type'], sub);
+
+	      element.appendChild(sub);
+	    }
+	  }, {
+	    key: 'addRated',
+	    value: function addRated(rated, element) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, 'Rated', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = rated;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addRuntime',
+	    value: function addRuntime(runtime, element) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, 'Runtime', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = runtime;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addType',
+	    value: function addType(type, element) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, 'Type', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = 'Type: ' + type;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addGenre',
+	    value: function addGenre(genre, element) {
+	      var span = this.tool.newElement('div');
+	      this.tool.elementNames(span, 'Genre', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = 'Genre: ' + genre;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addActors',
+	    value: function addActors(actors, element) {
+	      var span = this.tool.newElement('div');
+	      this.tool.elementNames(span, 'Actors', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = 'Cast: ' + actors;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addPlot',
+	    value: function addPlot(plot, element) {
+	      var span = this.tool.newElement('div');
+	      this.tool.elementNames(span, 'Plot', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = plot;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addAwards',
+	    value: function addAwards(awards, element) {
+	      var span = this.tool.newElement('div');
+	      this.tool.elementNames(span, 'Awards', null);
+	      var p = this.tool.newElement('p');
+	      p.innerHTML = awards;
+	      span.appendChild(p);
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'addStreaming',
+	    value: function addStreaming(title, element) {
+	      var _this = this;
+
+	      title = title.toUpperCase();
+	      var data = JSON.parse(sessionStorage[title]);
+	      var span = this.tool.newElement('div');
+	      this.tool.elementNames(span, 'Streams', null);
+	      data.map(function (s) {
+	        return _this.streamLink(s, span);
+	      });
+
+	      element.appendChild(span);
+	      return span;
+	    }
+	  }, {
+	    key: 'streamLink',
+	    value: function streamLink(stream, element) {
+	      var span = this.tool.newElement('span');
+	      this.tool.elementNames(span, null, 'service');
+
+	      var a = this.tool.newElement('a');
+	      a.href = stream['url'];
+	      a.innerHTML = stream['service'];
+	      span.appendChild(a);
+
+	      element.appendChild(span);
+	      return span;
 	    }
 	  }]);
 
 	  return Movie;
 	}();
 
+	// div
+	// span poster
+	// span details
+	// div container
+	// span title
+	// span year
+	// div container
+	// span rated
+	// span runtime
+	// span type
+	// div Genre
+	// div actors
+	// div plot
+	// div awards
+	// div streaming
+
+
+	// {
+	//     "data": {
+	//         "id": "0",
+	//         "type": "movie",
+	//         "attributes": {
+	//             "id": 0,
+	//             "details": {
+	//                 "Title": "BoJack Horseman",
+	//                 "Year": "2014–",
+	//                 "Rated": "TV-MA",
+	//                 "Runtime": "25 min",
+	//                 "Genre": "Animation, Comedy, Drama",
+	//                 "Director": "N/A",
+	//                 "Actors": "Will Arnett, Amy Sedaris, Alison Brie, Aaron Paul",
+	//                 "Plot": "BoJack Horseman was the star of the hit TV show \"Horsin' Around\" in the '90s, now he's washed up, living in Hollywood, complaining about everything, and wearing colorful sweaters.",
+	//                 "Awards": "Nominated for 1 Primetime Emmy. Another 7 wins & 20 nominations.",
+	//                 "Poster": "https://m.media-amazon.com/images/M/MV5BNzgzNzg3MDkyOF5BMl5BanBnXkFtZTgwOTk5MjAzNjM@._V1_SX300.jpg",
+	//                 "imdbRating": "8.5/10",
+	//                 "Type": "series"
+	//             }
+	//         }
+	//     }
+	// }
+
+
 	exports.default = Movie;
 
 /***/ }),
-/* 7 */
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
